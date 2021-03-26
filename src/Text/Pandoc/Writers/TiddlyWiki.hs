a5fffa96f6629f676f8b9abefcd9487df3b61275
Josh Kunz <josh@kunz.xyz>
2021-03-26 16:58:52 -0700

    Support `Null` blocks.

--- src/Text/Pandoc/Writers/TiddlyWiki.hs
+++ src/Text/Pandoc/Writers/TiddlyWiki.hs
@@ -4,9 +4,16 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Text.Pandoc.Class.PandocMonad (PandocMonad)
 import Text.Pandoc.Options (WriterOptions)
-import Text.Pandoc.Definition (Pandoc)
+import Text.Pandoc.Definition
 
 -- | Convert Pandoc to TiddlyWiki.
 writeTiddlyWiki:: PandocMonad m => WriterOptions -> Pandoc -> m Text
-writeTiddlyWiki _ _ =
-    return $ T.pack "Hello Pandoc"
+writeTiddlyWiki _ (Pandoc _ blocks) =
+    return . mconcat . map (endBlock . writeBlock) $ blocks
+    where endBlock t = if T.null t then t else t <> (T.pack "\n")
+
+writeBlock :: Block -> Text
+writeBlock Null = T.empty
+
+-- TODO(jkz): Handle all cases.
+writeBlock _ = T.empty
