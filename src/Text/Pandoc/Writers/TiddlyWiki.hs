ec758b6c4a43ef3518a663a97d96029b56177d66
Josh Kunz <josh@kunz.xyz>
2021-03-26 18:27:34 -0700

    Implement inline `Str`.

--- src/Text/Pandoc/Writers/TiddlyWiki.hs
+++ src/Text/Pandoc/Writers/TiddlyWiki.hs
@@ -14,16 +14,14 @@ writeTiddlyWiki:: PandocMonad m => WriterOptions -> Pandoc -> m Text
 writeTiddlyWiki _ (Pandoc _ blocks) = writeBlocks blocks
 
 writeBlocks :: PandocMonad m => [Block] -> m Text
-writeBlocks blocks =
-    (fmap mconcat) . mapM ((fmap endBlock) . writeBlock) $ blocks
-    where endBlock t = if T.null t then t else t <> "\n\n"
+writeBlocks blocks = (fmap mconcat) . mapM writeBlock $ blocks
 
 writeBlock :: PandocMonad m => Block -> m Text
 
 writeBlock Null = return T.empty
 
 -- https://tiddlywiki.com/#Horizontal%20Rules%20in%20WikiText
-writeBlock HorizontalRule = return "---"
+writeBlock HorizontalRule = return "\n---\n"
 
 -- https://tiddlywiki.com/#HTML%20in%20WikiText
 writeBlock (RawBlock f text)
@@ -31,5 +29,20 @@ writeBlock (RawBlock f text)
     | f == Format "tiddlywiki" = return $ text <> "\n"
 writeBlock b@(RawBlock _ _) = T.empty <$ report (BlockNotRendered b)
 
+writeBlock (Plain inlines) = writeInlines inlines
+writeBlock (Para inlines) = (<> "\n\n") <$> writeInlines inlines
+
 -- TODO(jkz): Handle all cases.
 writeBlock _ = return T.empty
+
+writeInlines :: PandocMonad m => [Inline] -> m Text
+writeInlines inlines =
+    (fmap mconcat) . mapM ((fmap endInline) . writeInline) $ inlines
+    where endInline i = if T.null i then i else i <> " "
+
+writeInline :: PandocMonad m => Inline -> m Text
+
+writeInline (Str t) = return t
+
+-- TODO(jkz): Handle all inlines.
+writeInline _ = return T.empty
