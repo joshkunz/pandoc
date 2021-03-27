429f2f82f056853ee66c382ca43c0d624e3e693e
Josh Kunz <josh@kunz.xyz>
2021-03-26 17:02:30 -0700

    Support Horizontal Rules

--- src/Text/Pandoc/Writers/TiddlyWiki.hs
+++ src/Text/Pandoc/Writers/TiddlyWiki.hs
@@ -10,10 +10,13 @@ import Text.Pandoc.Definition
 writeTiddlyWiki:: PandocMonad m => WriterOptions -> Pandoc -> m Text
 writeTiddlyWiki _ (Pandoc _ blocks) =
     return . mconcat . map (endBlock . writeBlock) $ blocks
-    where endBlock t = if T.null t then t else t <> (T.pack "\n")
+    where endBlock t = if T.null t then t else t <> (T.pack "\n\n")
 
 writeBlock :: Block -> Text
 writeBlock Null = T.empty
+
+-- https://tiddlywiki.com/#Horizontal%20Rules%20in%20WikiText
+writeBlock HorizontalRule  = T.pack "---"
 
 -- TODO(jkz): Handle all cases.
 writeBlock _ = T.empty
