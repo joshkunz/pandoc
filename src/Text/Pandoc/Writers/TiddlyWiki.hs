7af4a30df60a387e8eaf43323872403ac67b6a0f
Josh Kunz <josh@kunz.xyz>
2021-03-26 18:03:26 -0700

    Support RawBlock.

--- src/Text/Pandoc/Writers/TiddlyWiki.hs
+++ src/Text/Pandoc/Writers/TiddlyWiki.hs
@@ -1,22 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Text.Pandoc.Writers.TiddlyWiki ( writeTiddlyWiki ) where
 
 import Data.Text (Text)
 import qualified Data.Text as T
-import Text.Pandoc.Class.PandocMonad (PandocMonad)
+import Text.Pandoc.Class.PandocMonad (PandocMonad, report)
 import Text.Pandoc.Options (WriterOptions)
 import Text.Pandoc.Definition
+import Text.Pandoc.Logging (LogMessage(..))
 
 -- | Convert Pandoc to TiddlyWiki.
 writeTiddlyWiki:: PandocMonad m => WriterOptions -> Pandoc -> m Text
-writeTiddlyWiki _ (Pandoc _ blocks) =
-    return . mconcat . map (endBlock . writeBlock) $ blocks
-    where endBlock t = if T.null t then t else t <> (T.pack "\n\n")
+writeTiddlyWiki _ (Pandoc _ blocks) = writeBlocks blocks
 
-writeBlock :: Block -> Text
-writeBlock Null = T.empty
+writeBlocks :: PandocMonad m => [Block] -> m Text
+writeBlocks blocks =
+    (fmap mconcat) . mapM ((fmap endBlock) . writeBlock) $ blocks
+    where endBlock t = if T.null t then t else t <> "\n\n"
+
+writeBlock :: PandocMonad m => Block -> m Text
+
+writeBlock Null = return T.empty
 
 -- https://tiddlywiki.com/#Horizontal%20Rules%20in%20WikiText
-writeBlock HorizontalRule  = T.pack "---"
+writeBlock HorizontalRule = return "---"
+
+-- https://tiddlywiki.com/#HTML%20in%20WikiText
+writeBlock (RawBlock f text)
+    | f == Format "html" = return $ text <> "\n"
+    | f == Format "tiddlywiki" = return $ text <> "\n"
+writeBlock b@(RawBlock _ _) = T.empty <$ report (BlockNotRendered b)
 
 -- TODO(jkz): Handle all cases.
-writeBlock _ = T.empty
+writeBlock _ = return T.empty
